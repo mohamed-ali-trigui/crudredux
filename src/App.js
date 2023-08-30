eb3d84e50af05ed28a9bd61bae795d7245c0a8a6
unknown <Mohamed.Ali.Trigui@DigiCross.fr>
2023-08-30 15:45:55 +0200

    Commit new version

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Listarticles from './components/articlesRedux/Listarticles';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import ProductsAppAdmin from './admin/components/articles/ProductsAppAdmin';
+import Navscroll from './components/Navscroll';
 
 function App() {
   return (
     <>
     <Router>
+      <Navscroll/>
       <Routes>
     
       <Route path='/articlesadmin' element={<Listarticles/>}/> 
